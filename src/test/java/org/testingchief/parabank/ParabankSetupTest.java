74683b24aa8edd1981f48acdc1e75a73937a64d3
SK <skthetester@gmail.com>
2023-02-12 17:42:23 -0500

    added dataprovider

--- src/test/java/org/testingchief/parabank/ParabankSetupTest.java
+++ src/test/java/org/testingchief/parabank/ParabankSetupTest.java
@@ -5,52 +5,37 @@ import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 
 public class ParabankSetupTest {
+    private static final String BASE_URL = "http://192.168.0.182:8080/parabank/services/bank/";
+
     @Test(priority = 1)
     public void shutdownJmsListener() {
-        given().
-        when().
-            post("http://192.168.0.182:8080/parabank/services/bank/shutdownJmsListener").
-        then().
-            statusCode(204).log().all();
+        given().when().post(BASE_URL + "shutdownJmsListener").then()
+                .statusCode(204).log().all();
     }
 
     @Test(priority = 2)
     public void startupJmsListener() {
-        given().
-        when().
-            post("http://192.168.0.182:8080/parabank/services/bank/startupJmsListener").
-        then().
-            statusCode(204).log().all();
+        given().when().post(BASE_URL + "startupJmsListener").then()
+                .statusCode(204).log().all();
     }
 
     @Test(priority = 3)
     public void cleanDB() {
-        given().
-        when().
-            post("http://192.168.0.182:8080/parabank/services/bank/cleanDB").
-        then().
-            statusCode(204).
-            log().all();
+        given().when().post(BASE_URL + "cleanDB").then().statusCode(204).log()
+                .all();
     }
 
     @Test(priority = 4)
     public void initializeDB() {
-        given().
-        when().
-            post("http://192.168.0.182:8080/parabank/services/bank/initializeDB").
-        then().
-            statusCode(204).
-            log().all();
+        given().when().post(BASE_URL + "initializeDB").then().statusCode(204)
+                .log().all();
     }
 
     @Test(priority = 5)
     public void setParameter() {
-        given().
-        when().
-            post("http://192.168.0.182:8080/parabank/services/bank/setParameter/" + "NewParam" + "/" + "1234").
-        then().
-            statusCode(204).
-            log().all();
+        given().when()
+                .post(BASE_URL + "setParameter/" + "NewParam" + "/" + "1234")
+                .then().statusCode(204).log().all();
     }
 
 }
--- src/test/java/org/testingchief/zippopotam/ZipCodeTest.java
+++ src/test/java/org/testingchief/zippopotam/ZipCodeTest.java
@@ -1,5 +1,6 @@
 package org.testingchief.zippopotam;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -8,56 +9,43 @@ import static org.hamcrest.Matchers.*;
 public class ZipCodeTest {
     private static final String BASE_URL = "http://api.zippopotam.us/";
 
-    //simple test
+    // simple test
     @Test(priority = 1)
     public void requestUSZipCode() {
-        given().
-        when().
-            get(BASE_URL + "us/90210").
-        then().
-            statusCode(200).
-            assertThat().
-                body("places[0].'place name'", equalTo("Beverly Hills")).
-            log().all();
+        given().when().get(BASE_URL + "us/90210").then().statusCode(200).assertThat()
+                .body("places[0].'place name'", equalTo("Beverly Hills")).log().all();
     }
 
-    //using path parameters
+    // using path parameters
     @Test(priority = 2)
     public void requestCAZipCode() {
-        given().
-            pathParam("countryCode", "ca").
-            pathParam("zipCode", "L1R").
-        when().
-            get(BASE_URL + "{countryCode}/{zipCode}").
-        then().
-            statusCode(200).
-            assertThat().
-                body("places[0].'place name'", equalTo("Whitby Central")).
-            log().all();
+        given().pathParam("countryCode", "ca").pathParam("zipCode", "L1R").when()
+                .get(BASE_URL + "{countryCode}/{zipCode}").then().statusCode(200).assertThat()
+                .body("places[0].'place name'", equalTo("Whitby Central")).log().all();
     }
 
-    //using data provider
-    @Test(priority = 3)
-    public void requestUSCityName() {
-        given().
-        when().
-            get("http://api.zippopotam.us/us/oh/dayton").
-        then().
-            statusCode(200).
-            assertThat().
-                body("places[0].'place name'", equalTo("Dayton")).
-            log().all();
+    // using data provider
+    @DataProvider(name = "US_CITIES")
+    public Object[][] cityData() {
+        return new Object[][] {
+                { "us", "45342", "Dayton" },
+                { "us", "90210", "Beverly Hills" },
+                { "ca", "L1R", "Whitby" }
+        };
+    }
+
+    @Test(priority = 3, dataProvider = "US_CITIES")
+    public void requestCityName(String countryCode, String zipCode, String cityName) {
+        given().pathParam("countryCode", countryCode)
+                .pathParam("zipCode", zipCode)
+                .pathParam("cityName", cityName).when().get(BASE_URL + "{countryCode}/{zipCode}").then().statusCode(200)
+                .assertThat()
+                .body("places[0].'place name'", containsString("{cityName}")).log().all();
     }
 
     @Test(priority = 4)
     public void requestCACityName() {
-        given().
-        when().
-            get("http://api.zippopotam.us/ca/on/ajax").
-        then().
-            statusCode(200).
-            assertThat().
-                body("places[0].'place name'", containsString("Ajax")).
-            log().all();
+        given().when().get(BASE_URL + "ca/on/ajax").then().statusCode(200).assertThat()
+                .body("places[0].'place name'", containsString("Ajax")).log().all();
     }
 }
