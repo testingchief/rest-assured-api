92aebce51d04d8774e2bc9c3e54755b5c236d6ea
skthetester <skthetester@gmail.com>
2023-07-28 13:52:47 -0400

    Rename SetupTest

--- src/test/java/org/testingchief/parabank/SetupTest.java
+++ src/test/java/org/testingchief/parabank/SetupTest.java
@@ -1,10 +1,11 @@
 package org.testingchief.parabank;
 
 import org.testng.annotations.Test;
+import com.github.javafaker.*;
 
 import static io.restassured.RestAssured.given;
 
-public class ParabankSetupTest {
+public class SetupTest {
     private static final String BASE_URL = "http://192.168.0.182:8080/parabank/services/bank/";
 
     @Test(priority = 1)
@@ -32,9 +33,12 @@ public class ParabankSetupTest {
     }
 
     @Test(priority = 5)
-    public void setParameter() {
+    public void setParameters() {
+        Faker faker = new Faker();
+        String productName = faker.company().name();
+        String productId = faker.number().digits(6);
         given().when()
-                .post(BASE_URL + "setParameter/" + "NewParam" + "/" + "1234")
+                .post(BASE_URL + "setParameter/" + productName + "/" + productId)
                 .then().statusCode(204).log().all();
     }
 
